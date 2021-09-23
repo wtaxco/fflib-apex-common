06450adb96fdce5a8dfa565bc9bf3744455e70ab
Frans Flippo <fransf@vatit.com>
2021-09-23 11:53:56 +0200

    WCP-558, WOI-12 / WOI-38 Added WTax deploy scripts

---
+++ config.sh
@@ -0,0 +1,16 @@
+project_name="fflib-apex-common"
+project_description="FFLib Apex Common"
+
+source_dir=sfdx-source/apex-common
+
+# scratch
+scratch_org_alias=wtax-fflib-apex-common-scratch
+# dev
+dev_org_url=https://wtax--developer.my.salesforce.com/
+dev_org_alias=wtax-developer
+# uat
+uat_org_url=https://wtax--uat.my.salesforce.com/
+uat_org_alias=wtax-uat
+# prod
+prod_org_url=https://login.salesforce.com/
+prod_org_alias=wtax-prod
